9411803e2811f8897e649287d9f61cd3e48729df
chungphamhn <chungphamhn@gmail.com>
2022-04-28 23:07:22 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        <div className="App-header">
+          <img src="https://chartshots.ctradercloud.com/626abf7fb9265.png"/>
+          <p></p>
+          <img src="https://chartshots.ctradercloud.com/626ac0c4babd2.png"/>
+        </div>
+        
     </div>
   );
 }
