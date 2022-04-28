1ef5e6666acd787842a19e34c714c692076ef0ae
chungphamhn <chungphamhn@gmail.com>
2022-04-28 23:39:51 +0300

    test

--- src/App.js
+++ src/App.js
@@ -10,6 +10,12 @@ function App() {
           <p></p>
           <img src="https://chartshots.ctradercloud.com/626ac0c4babd2.png"/>
         </div>
+        <div className="App-header">
+          <img src="https://chartshots.ctradercloud.com/626abf7fb9265.png"/>
+          <p></p>
+          <img src="https://chartshots.ctradercloud.com/626ac0c4babd2.png"/>
+        </div>
+        
         
     </div>
   );
