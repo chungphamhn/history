3910720d5c273a78505c5c531be60c725ac4f059
chungphamhn <chungphamhn@gmail.com>
2022-04-28 23:51:04 +0300

    add date to pictures

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ function App() {
     <div className="App">
       
         <div className="App-header">
+        <p></p>
+        <p>28 April 2022</p>
           <img src="https://chartshots.ctradercloud.com/626abf7fb9265.png"/>
-          <p></p>
+          <p>27 April 2022</p>
           <img src="https://chartshots.ctradercloud.com/626ac0c4babd2.png"/>
         </div>
         
